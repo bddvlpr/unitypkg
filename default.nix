2455eac2debd25edb36b445e4679851c4d359184
Luna Simons <luna@bddvlpr.com>
2024-06-27 02:42:31 +0200

    refactor: use workspace to separate bin and core

--- default.nix
+++ default.nix
@@ -2,21 +2,21 @@
   rustPlatform,
   lib,
 }: let
-  inherit ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package) version;
+  inherit ((builtins.fromTOML (builtins.readFile ./unitypkg-cli/Cargo.toml)).package) version;
 in
   rustPlatform.buildRustPackage {
-    pname = "unitypkg";
+    pname = "unitypkg-cli";
     inherit version;
 
     src = ./.;
 
-    cargoHash = "sha256-HB8We2K0D4xZFhGdy4505dxve2EGbuhJHFSkso7NZEU=";
+    cargoHash = "sha256-w5BshLg1OiqiJJ+JRm9JQkjMtWAex/eMtHmlOfQ7Kt4=";
 
     meta = with lib; {
       description = "Manipulate Unity's portable package files";
       homepage = "https://github.com/bddvlpr/unitypkg";
       license = licenses.mit;
       maintainers = with maintainers; [bddvlpr];
-      mainProgram = "unitypkg";
+      mainProgram = "unitypkg-cli";
     };
   }
